62734810248bcc3dd0383010a6c858ed1b006dd7
Dev-SIMONONI <bankislimbo18@gmail.com>
2025-07-15 21:04:30 +0100

    prompt user to enter details

--- basUserdetail.py
+++ basUserdetail.py
@@ -1,5 +1,5 @@
 #BASIC USER DETAILS DISPLAY
-Name="JOHN WICK"
-Age=34
-Height=6.7
+Name=input("enter full name \n >> ")
+Age=input("enter AGE \n >> ")
+Height=input("enter height in feets \n >> ")
 print(Name,Age,Height)
